16eca9db23b9f63c32d71b99fda1d04f70ddc22a
venkatesansubramanian <venkatesangit80@outlook.com>
2020-07-16 14:32:37 -0500

    Multiway cpu burner

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ def get_new_window():
     return new_window
 
 
+def get_new_window_More_CPU():
+    server = lbtmux.Server()
+    new_session = server.new_session(session_name="Multi_While_Session_More")
+    new_window = new_session.new_window(window_name="Milti_While_Window_More", attach=False)
+    return new_window
+
+
+def get_session_More():
+    server = lbtmux.Server()
+    exist_session = server.find_where({"session_name": "Multi_While_Session_More"})
+    return exist_session
+
+
 def get_session():
     server = lbtmux.Server()
     exist_session = server.find_where({"session_name": "Multi_While_Session"})
@@ -20,12 +33,25 @@ def get_session():
 def remove_session():
     current_session = get_session()
     current_session.kill_window("Milti_While_Window")
+    current_session.kill_session()
 
 
 def run_application():
     new_window = get_new_window()
-    command = "while true; do echo hello; done"
+    command = "while true; do echo hello; sleep .01; done"
     new_window.select_pane('0').send_keys(command, enter=True)
+
+
+def run_application_more():
+    new_window = get_new_window_More_CPU()
+    command = "while true; do echo hello; sleep .005; done"
+    new_window.select_pane('0').send_keys(command, enter=True)
+
+
+def remove_session_more():
+    current_session = get_session_More()
+    current_session.kill_window("Milti_While_Window_More")
+    current_session.kill_session()
 
 
 @app.route('/')
@@ -35,14 +61,38 @@ def hello_world():
 
 @app.route('/run_application')
 def application():
-    run_application()
-    return "success"
+    try:
+        run_application()
+        return "success"
+    except:
+        return "failure"
 
 
 @app.route('/kill_application')
 def kill():
-    remove_session()
-    return "success"
+    try:
+        remove_session()
+        return "success"
+    except:
+        return "failure"
+
+
+@app.route('/run_application_more')
+def application_more():
+    try:
+        run_application_more()
+        return "success"
+    except:
+        return "failure"
+
+
+@app.route('/kill_application_more')
+def kill_more():
+    try:
+        remove_session_more()
+        return "success"
+    except:
+        return "failure"
 
 
 if __name__ == '__main__':
